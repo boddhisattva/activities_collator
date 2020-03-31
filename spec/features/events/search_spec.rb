bebbf86eaf0279acd6df5a750e93e4dc3eb8b39a
Mohnish G J <mail4mohnish@gmail.com>
2020-03-31 20:58:53 +0200

    Add spec for filter search by multiple filter criteria
    - Also ensure that for another spec it only shows matching results and
    not matching details are not displayed as part of resultant search page

--- spec/features/events/search_spec.rb
+++ spec/features/events/search_spec.rb
@@ -29,6 +29,7 @@ RSpec.feature 'Events search', type: :feature do
         click_button 'Search'
 
         expect(page).to have_text('Near future event')
+        expect(page).not_to have_text('Later event')
       end
 
       scenario 'event start date is greater event finish date' do
@@ -45,6 +46,27 @@ RSpec.feature 'Events search', type: :feature do
 
         expect(page).to have_text('Start date cannot be greater than finish date')
         expect(page).to have_css('.alert-danger')
+      end
+
+      scenario 'user searches with multiple search filter criteria' do
+        websource = create :web_source, name: 'new websource'
+        another_websource = create :web_source, new: 'another websource'
+        event_in_near_future = create :event, title: 'Near future event', start: 2.weeks.from_now, finish: 3.weeks.from_now, web_source: websource
+        event_for_much_later = create :event, title: 'Later Event', start: 2.months.from_now, finish: 3.months.from_now, web_source: another_websource
+
+        visit root_path
+
+        within('.filters') do
+          fill_in 'events[title]', with: 'near future'
+          fill_in 'events[start]', with: 1.week.from_now.strftime('%m/%d/%Y')
+          fill_in 'events[finish]', with: 2.months.from_now.strftime('%m/%d/%Y')
+          select('new websource', from: 'events[web_source_id]')
+        end
+
+        click_button 'Search'
+
+        expect(page).to have_text('Near future event')
+        expect(page).not_to have_text('later event')
       end
     end
   end
