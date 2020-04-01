80d21616e35059d6eace01176e937202c03413b6
Mohnish G J <mail4mohnish@gmail.com>
2020-04-01 13:44:08 +0200

    Raise error if event URL not defined in a subclass

--- spec/scrapers/base_scraper_spec.rb
+++ spec/scrapers/base_scraper_spec.rb
@@ -1,8 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe BaseScraper do
-  context 'Page has duplicate events on the page' do
-    it 'captures appropriate errors as part of event creation' do
+  class BrandNewScraper < BaseScraper; end
+
+  describe '#scrape' do
+    let(:web_source) { create(:web_source) }
+    let(:new_scraper) { BrandNewScraper.new }
+
+    context 'event URL not defined in the subclass' do
+      it 'raises a NotImplementedError without an appropriate error message' do
+        expect { new_scraper.scrape(web_source) }.to raise_error(NotImplementedError, 'An event URL needs to be present in a subclass')
+      end
     end
   end
 end
